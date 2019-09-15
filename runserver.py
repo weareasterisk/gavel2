02e4228dedc80e20fa1fe4b7808319901cfc8045
Muhammad Khatri <muhammad@weareasterisk.com>
2019-09-15 02:28:16 -0500

    fix: run flask in threaded mode

--- runserver.py
+++ runserver.py
@@ -18,5 +18,6 @@ if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
         port=PORT,
-        extra_files=extra_files
+        extra_files=extra_files,
+        threaded=True
     )
